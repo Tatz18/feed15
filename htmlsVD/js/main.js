ef79806fc3eedbaa36202110afec67c8d4c6cb9c
Shouvik Bhuiyan <shouvikbhuiyan1990@gmail.com>
2015-01-29 23:32:03 +0530

    mobile only

--- htmlsVD/js/main.js
+++ htmlsVD/js/main.js
@@ -1,5 +1,30 @@
 var HomePageControls = {
 	navControl:function(){
+		$('#feed-search').click(function(e){
+			e.stopPropagation();
+			$('#feed-dd1').slideDown();
+			$('.feed-nav').slideUp();
+		});
+		$(document).click(function(){
+			HomePageControls.hideDropDown();
+
+		});
+		$('#feed-dd1,#feed-dd2').click(function(e){
+			e.stopPropagation();			
+		});
+		$('#feed-filter').click(function(e){
+			e.stopPropagation();
+			$('#feed-dd2').slideDown();
+			$('.feed-nav').slideUp();
+		});
+		$('.feed-back-btn').click(function(){
+			HomePageControls.hideDropDown();
+			$('.feed-home .feed-nav').slideDown();
+
+		});
+	},
+	hideDropDown:function(){
+		$('#feed-dd1,#feed-dd2').slideUp();
 	}
 }
 ;
